8457942ca64f05e00f97400af6e57f571ff87468
Bruna Borges <brunaborgesdev@gmail.com>
2024-06-04 09:36:25 -0300

    Adjust Home for desktop screens

--- bruborgesdev/src/App.tsx
+++ bruborgesdev/src/App.tsx
@@ -1,15 +1,15 @@
 import { Header } from "./components/Header"
-import { Footer } from "./components/Footer"
 import { CoreContent } from "./pages/CoreContent"
+import { Footer } from "./components/Footer"
 
 export const App = () => {
   return (
     <div className='font-body text-cl-white min-h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-cl-purple via-cl-purple2 to-cl-full-black'>
       <div>
-      <Header />
-      <CoreContent />
+        <Header />
+        <CoreContent />
+        <Footer />
       </div>
-      <Footer />
     </div>
   )
 }
--- bruborgesdev/src/pages/Home.tsx
+++ bruborgesdev/src/pages/Home.tsx
@@ -1,16 +1,16 @@
 export const Home = () => {
     return (
-        <div id="home" className="mt-16 h-screen">
-            <div className="flex flex-col items-center justify-center gap-4">
-                <div className="flex flex-col max-w-48">
+        <div id="home" className="mt-16 h-screen md:h-full">
+            <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-10">
+                <div className="flex flex-col max-w-48 md:max-w-80">
                     <span className="text-left text-cl-green"> {'<developer>'} </span>
-                    <h1 className="text-6xl font-title">Bruna Borges</h1>
+                    <h1 className="text-6xl font-title md:text-5xl">Bruna Borges</h1>
                     <span className="text-right pt-2 text-cl-green"> {'<developer>'} </span>
                 </div>
-                <img className="w-72 h-72" src="src/assets/profileimg.png" alt="" />
-                <div className="flex flex-col text-center ">
-                    <h2 className="text-3xl font-title text-cl-green"> Full-stack Developer</h2>
-                    <span className="text-lg">Desenvolvendo aplicações web robustas usando React, Node.js, Tailwind CSS, entre outros...</span>
+                <img className="w-72 h-72 md:w-80 md:h-80" src="src/assets/profileimg.png" alt="" />
+                <div className="flex flex-col text-center md:max-w-64">
+                    <h2 className="text-3xl font-title text-cl-green">Full-stack Developer</h2>
+                    <span className="text-lg md:text-justify">Desenvolvendo aplicações web utilizando React no front e Node.js no back</span>
                 </div>
             </div>
 
