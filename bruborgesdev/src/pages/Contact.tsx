87b819cc99d011bf371bf943f2fa574849a71aee
Bruna Borges <brunaborgesdev@gmail.com>
2024-06-06 14:27:14 -0300

    set desktop screen for Contact

--- bruborgesdev/src/pages/Contact.tsx
+++ bruborgesdev/src/pages/Contact.tsx
@@ -2,10 +2,10 @@ import { SocialButton } from "../components/SocialButton"
 
 export const Contact = () => {
     return (
-        <div id="contact" className="h-screen mt-20">
+        <div id="contact" className="h-screen flex flex-col items-center justify-center md:h-full md:mb-32">
 
             <h1 className="text-4xl font-title text-cl-green flex justify-center pb-20">Contatos</h1>
-            <div className='flex flex-col items-center gap-24'>
+            <div className='flex flex-col items-center gap-24 md:flex-row md:justify-center'>
                 
                 <SocialButton text="@brunacborgesm" social="Linkedin" img="src/assets/linkedin.png" url={"https://www.linkedin.com/in/brunacborgesm/"} />
 
