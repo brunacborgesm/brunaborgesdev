643ef9a0b6c8bd90e4e47b2dadfdd27b9ceed01c
Bruna Borges <brunaborgesdev@gmail.com>
2024-06-22 14:08:56 -0300

    feat: about me page edited

--- bruborgesdev/src/pages/AboutMe.tsx
+++ bruborgesdev/src/pages/AboutMe.tsx
@@ -7,9 +7,9 @@ export const AboutMe = () => {
             <h1 className="text-4xl font-title text-cl-green text-center pb-12 md:pb-20">Sobre mim</h1>
             <div className="flex flex-col items-center gap-14 md:flex-row lg:px-28 md:justify-center">
                 <section className="text-center leading-7 text-lg md:text-justify  md:w-3/6">
-                    <p>Desenvolvedora apaixonada por criar experiências digitais com três anos de experiência em tecnologia. Utilizo o ReactJS e estou aprimorando minhas habilidades em Back-end com Node.js para oportunidades como Full-Stack Developer.</p>
+                    <p>Desenvolvedora com três anos de experiência em tecnologia. Utilizo o ReactJS e estou aprimorando minhas habilidades em Back-end com Node.js para oportunidades como Full-Stack Developer.</p>
                     
-                    <p>Minha trajetória destaca-se pela colaboração eficaz e determinação no desenvolvimento front-end. Sou reconhecida por enfrentar desafios com determinação, sempre buscando excelência e sendo orientada para resultados.</p>
+                    <p>Minha trajetória destaca-se pela colaboração e determinação no desenvolvimento front-end. Orientada para resultados, sou reconhecida por enfrentar desafios com determinação e excelência.</p>
                 </section>
                 <aside className=" md:w-3/6">
                     <h1 className="text-3xl font-title text-cl-green flex justify-center pb-3">Techs</h1>
