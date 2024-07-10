5b07598be41a6c02d160430b756c3799fdd1ae6f
Bruna Borges <brunaborgesdev@gmail.com>
2024-07-10 20:17:45 -0300

    fix: deleting old imports

--- bruborgesdev/src/App.tsx
+++ bruborgesdev/src/App.tsx
@@ -2,10 +2,6 @@ import { Header } from "./components/Header"
 import { CoreContent } from "./pages/CoreContent"
 import { Footer } from "./components/Footer"
 
-import dots from "./assets/elements/dots.png"
-import ximage from "./assets/elements/x.png"
-import balls from "./assets/elements/balls.png"
-
 export const App = () => {
   return (
     <div className='font-body text-cl-white min-h-screen bg-custom-mb md:bg-custom-dk bg-cover bg-fixed bg-no-repeat'>
