74724f78cbe57bf3f0becf82a97bc4836c421c88
Bruna Borges <brunaborgesdev@gmail.com>
2024-07-10 20:35:35 -0300

    fix: bg size

--- bruborgesdev/src/App.tsx
+++ bruborgesdev/src/App.tsx
@@ -4,7 +4,7 @@ import { Footer } from "./components/Footer"
 
 export const App = () => {
   return (
-    <div className='font-body text-cl-white min-h-screen bg-custom-mb md:bg-custom-dk bg-cover bg-fixed bg-no-repeat'>
+    <div className='font-body text-cl-white min-h-screen bg-custom-mb md:bg-custom-dk lg:bg-custom-dk bg-cover bg-fixed bg-no-repeat'>
       <div>
         <Header />
         <CoreContent />
--- bruborgesdev/src/components/TechTag.tsx
+++ bruborgesdev/src/components/TechTag.tsx
@@ -4,7 +4,7 @@ interface TechTagProps {
 
 export const TechTag:React.FC<TechTagProps> = ({ tech }) => {
     return (
-        <div className="px-2 bg-cl-green text-cl-purple-2 rounded-md text-sm">
+        <div className="px-2 bg-cl-green text-cl-purple-2 rounded-md text-xs">
             <span>{ tech }</span>
         </div>
     )
