27714259b40ddcdbb39f2733009bfbadf5dfc90a
Bruna Borges <brunaborgesdev@gmail.com>
2024-07-10 22:03:01 -0300

    fix: mobile screen

--- bruborgesdev/src/pages/AboutMe.tsx
+++ bruborgesdev/src/pages/AboutMe.tsx
@@ -4,7 +4,7 @@ import { TechSlider } from "../components/TechSlider"
 
 export const AboutMe = () => {
     return (
-        <div id="about_me" className="h-screen flex flex-col justify-center md:h-full md:mt-20">
+        <div id="about_me" className="min-h-screen flex flex-col justify-center">
             <h1 className="text-4xl font-title text-cl-green text-center pb-12 md:pb-20">Sobre mim</h1>
             <div className="flex flex-col items-center gap-14 md:flex-row lg:px-28 md:justify-center">
                 <section className="text-center leading-7 text-lg md:text-justify  md:w-3/6">
--- bruborgesdev/src/pages/Hero.tsx
+++ bruborgesdev/src/pages/Hero.tsx
@@ -2,29 +2,33 @@ import profileimg from "../assets/profileimg.png"
 import { DefaultButton } from "../components/DefaultButton"
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import cv from "../assets/doc/cv-pt-br.pdf"
+import { KeyboardDoubleArrowDown } from "@mui/icons-material";
 
 export const Hero = () => {
     return (
-        <div className="my-20 h-full">
+        <div className="mt-20 min-h-screen">
             <div className="flex flex-col items-center justify-center gap-4 md:gap-7 md:flex-row">
 
-                <div className="flex flex-col max-w-40 md:max-w-40 xl:max-w-96">
+                <div className="flex flex-col max-w-40 md:max-w-40 xl:max-w-96 ">
                     <span className="text-left text-cl-green"> {'<developer>'} </span>
                     <h1 className="text-5xl font-title md:max-w-64 xl:max-w-96">Bruna Borges</h1>
                     <span className="text-right mt-2 text-cl-green"> {'</developer>'} </span>
                 </div>
 
-                <img className="w-max-w-96 h-max-w-96 md:w-80 md:h-80" src={ profileimg } alt="" />
+                <img className="w-max-w-96 h-max-w-96 md:w-80 md:h-80" src={profileimg} alt="" />
 
                 <div className="flex flex-col text-center md:max-w-64 xl:max-w-96">
                     <h2 className="text-3xl font-title text-cl-green">Full-stack Developer</h2>
+                    <p>Profissional pronta para atender suas necessidades tecnológicas</p>
                     <div className="mt-5">
-                        <DefaultButton text="Download CV" icon={ <FileDownloadOutlinedIcon /> } url={ cv }/>
+                        <DefaultButton text="Download CV" icon={<FileDownloadOutlinedIcon />} url={cv} />
                     </div>
                 </div>
 
+            </div>
 
-
+            <div className="mt-20 h-32 animate-bounce text-cl-lavander flex justify-center">
+                <KeyboardDoubleArrowDown />
             </div>
 
         </div>
