01a8186c70559fc842b909d7310597bbf63ae9f9
Bruna Borges <brunaborgesdev@gmail.com>
2024-08-28 10:44:48 -0300

    fix: bg mb/dk

--- bruborgesdev/src/App.tsx
+++ bruborgesdev/src/App.tsx
@@ -4,7 +4,10 @@ import { Footer } from "./components/Footer"
 
 export const App = () => {
   return (
-    <div className='font-body text-cl-white min-h-screen bg-custom-mb md:bg-custom-dk lg:bg-custom-dk bg-cover bg-fixed bg-no-repeat sm:bg-cover'>
+    <div className='font-body text-cl-white min-h-screen bg-custom-mb md:bg-custom-dk lg:bg-custom-dk
+     bg-cover
+     bg-fixed
+     bg-no-repeat'>
       <div>
         <Header />
         <CoreContent />
